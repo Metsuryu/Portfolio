deeba0bfdaed70ddfe44cab82f63c9145520b8bb
Mario Cannistrà <Metsuryu@users.noreply.github.com>
2017-06-05 17:35:42 +0200

    Change layout

--- js/themes.js
+++ js/themes.js
@@ -1,11 +1,12 @@
 function toLight(){
+	$("#toggleLights").attr("onclick","toDark()");
 	$("body").css({
 		"color":"#2d2e2e",
-		"background-color":"white"
+		"background-color":"#fff"
 	});
 	$("p").css("color","black");
 	$("a").css("color","rgb(51, 122, 183)");
-	$("#githubID").css("color","#2d2e2e");
+	$("#githubID").css("color","#000");
 	$(".gridSquare h3 a").css("color","rgb(51, 122, 183)");
 	$("hr").css("border-color","#2d2e2e");
 	$("#contacts").css({
@@ -14,9 +15,11 @@ function toLight(){
 	});
 	$("#contacts a p").css("color","rgb(51, 122, 183)");
 	$(".gridSquare").css("background-color","white");
+	$("li").css("color","#2d2e2e");
 }
 
 function toDark(){
+	$("#toggleLights").attr("onclick","toLight()");
 	$("body").css({
 		"color":"rgb(0, 216, 255)",
 		"background-color":"rgb(21, 21, 21)"
@@ -32,4 +35,5 @@ function toDark(){
 	});
 	$("#contacts a p").css("color","rgb(0, 216, 255)");
 	$(".gridSquare").css("background-color","rgb(60, 60, 60)");
+	$("li").css("color","#fff");
 }
